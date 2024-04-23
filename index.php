a678d612ca86c7a84657d13dd7bf40c8208a3df7
Dale Kanikkeberg <kanikkeberg.dale@student.greenriver.edu>
2024-04-23 16:07:56 -0400

    filled with examples of arrays in fat-free

--- index.php
+++ index.php
@@ -13,7 +13,19 @@ require_once ('vendor/autoload.php');
 $f3 = Base::instance();
 
 // Define a default route
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
+
+    $f3->set('name', 'Winton');
+
+    $fruits = array('apple', 'orange', 'banana');
+    $f3->set('fruitList', $fruits);
+
+    $games = array('Splatoon 3', 'Fortnite', 'Overwatch');
+    $f3->set('gamesList', $games);
+
+    $students = array('101'=>'Tien', '102'=>'Colton', '103'=>'Sage');
+    $f3->set('studentList', $students);
+
     // Render a view page
     $view = new Template();
     echo $view->render('views/info.html');
